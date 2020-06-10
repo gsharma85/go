d4d99c71d79ba108c528ce0f84485101d7b3624a
gsharma85 <gaurav_sharmanitk@yahoo.com>
2020-06-10 15:23:53 +0530

    Added flags

--- sensor/cmd/sensor/Sensor.go
+++ sensor/cmd/sensor/Sensor.go
@@ -4,15 +4,20 @@ import (
 	. "github.com/gsharma85/go/sensor/internal/core"
 	. "github.com/gsharma85/go/sensor/internal/data"
 	"time"
+	"flag"
 )
 
 func main() {
+	
+	dir := flag.String("d", "directory", "directory to watch")
+	flag.Parse()
+	
 	stopChan := make(chan struct{})
 	keyGenFun := func(senseEvent SenseEvent) (groupByKey string, eventKey string) {
 		return senseEvent.Action,senseEvent.Key
 	}
 	
-	senseConfig := SenseConfig{stopChan, "C:\\gotest", 100 * time.Millisecond, keyGenFun}
+	senseConfig := SenseConfig{stopChan, *dir, 100 * time.Millisecond, keyGenFun}
 	
 	StartSensorPipeline("file", []string{"deduplicator"} , "console", senseConfig)
 	
