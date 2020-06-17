50e232a9c758372f49f448e431af7a0e93bcc387
gsharma85 <gaurav_sharmanitk@yahoo.com>
2020-06-17 14:32:31 +0530

    Actor System

--- actor/internal/core/actor/Actor.go
+++ actor/internal/core/actor/Actor.go
@@ -36,30 +36,40 @@ type Actor struct {
 	stopActorSystemSignal chan struct{} 
 }
 
-func CreateActor(name string, address string, commandProcessors map[string]func(ActorCommand, ActorState) ActorResponse, TimedCommands map[string]ActorCommand, stopActorSystemSignal chan struct{}) *Actor {
+func CreateActor(name string, address string, commandProcessors map[string]func(ActorCommand, ActorState) ActorResponse, timedCommands map[string]ActorCommand, stopActorSystemSignal chan struct{}) *Actor {
 	inChan := make(chan ActorCommand)
 	outChan := make(chan ActorResponse)
 	state := ActorState{make(map[string]interface{})}
-	processor := ActorProcessor{commandProcessors, TimedCommands}
+	processor := ActorProcessor{commandProcessors, timedCommands}
 	actor := Actor{name, address, processor, inChan, outChan, state, stopActorSystemSignal}
 	
-	// Load generic commands like buildActorState, passivateActor, getState
+	// Load generic commands like buildActorState, passivateActor, getState, pushStateOnChannel
 	
 	// Start Actor command processor
 	closeActorSelfGoRoutines := make(chan struct{})
+	createExternalCommandRoutine(&actor, closeActorSelfGoRoutines)
+	
+	
+	// Start actor self commands
+	createSelfCommandRoutines(timedCommands, actor.InChan, closeActorSelfGoRoutines)
+	
+	return &actor
+}
+
+func createExternalCommandRoutine(actorRef *Actor, closeActorSelfGoRoutines chan struct{}) {
 	go func(actor *Actor) {
 		for {
 			select {
 				case actorCommand, open := <-actor.InChan:
 				if !open {
-					close(inChan)
-					close(outChan)
+					close(actor.InChan)
+					close(actor.OutChan)
 					return
 				}
 				
 				processor := actor.processor.CommandProcessor[actorCommand.Name]
 				response := processor(actorCommand, actor.State)
-				outChan <- response
+				actor.OutChan <- response
 				
 				// Close all the self go routines if this was a passivate command
 				if actorCommand.Name == "passivateActor" {
@@ -69,25 +79,43 @@ func CreateActor(name string, address string, commandProcessors map[string]func(
 				
 				case _, open := <- actor.stopActorSystemSignal:
 				if !open {
-					close(inChan)
-					close(outChan)
+					close(actor.InChan)
+					close(actor.OutChan)
 					return
 				}
 			}
 		}
-	}(&actor)
-	
-	
-	// Start actor self commands
-	if TimedCommands != nil && len(TimedCommands) > 0 {
-		for name, command := range TimedCommands {
-			ticker := time.NewTicker(500 * time.Millisecond)
+	}(actorRef)
+}
+
+func createSelfCommandRoutines(timedCommands map[string]ActorCommand, actorInChan chan ActorCommand, closeActorSelfGoRoutines chan struct{}) {
+	if timedCommands != nil && len(timedCommands) > 0 {
+		for name, command := range timedCommands {
 			go func(name string, command ActorCommand, actorInChan chan ActorCommand, closeActorSelfGoRoutines chan struct{}) {
+				
+				timerResetFunc := func(scheduledOn time.Time) *time.Timer{
+					triggerDelay := scheduledOn.Sub(time.Now())
+					
+					var nextTriggerOn time.Duration
+					if triggerDelay <= 0 {
+						nextTriggerOn = time.Until(scheduledOn.Add(time.Hour * 24))
+					} else {
+						nextTriggerOn = time.Until(scheduledOn)
+					}
+					
+					log.Println("Next timer for command: %s will be in %s", name, nextTriggerOn)
+					
+					return time.NewTimer(nextTriggerOn)
+				}
+			
+				timer := timerResetFunc(command.CommandTime)
+			
 				for {
 					select {
-						case time,_ := <- ticker.C:
-						log.Println("Self command executed at %s", time)
+						case tickTime,_ := <- timer.C:
+						log.Println("Self command executed at %s", tickTime)
 						actorInChan <- command
+						timer = timerResetFunc(time.Now().Add(time.Hour * 24))
 						
 						case _, open := <- closeActorSelfGoRoutines:
 						if !open {
@@ -95,9 +123,7 @@ func CreateActor(name string, address string, commandProcessors map[string]func(
 						}
 					}
 				}
-			}(name, command, actor.InChan, closeActorSelfGoRoutines)
+			}(name, command, actorInChan, closeActorSelfGoRoutines)
 		}
 	}
-	
-	return &actor
 }
