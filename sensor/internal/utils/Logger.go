2139caa992cae7a1567b680e9a71a7847c25e77a
gsharma85 <gaurav_sharmanitk@yahoo.com>
2020-07-02 21:17:25 +0530

    sensor changes

---
+++ sensor/internal/utils/Logger.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+var logger *log.Logger
+func Logger(fileName string) *log.Logger {
+	
+	if logger == nil {
+		logFile, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+		   panic(err)
+		}		
+		logger = log.New(logFile, "", log.LstdFlags)
+	}
+	
+	return logger
+}
